6a8714f023c64054a1188abef4c6ff93f3ef6ac1
Mythili156 <153004073+Mythili156@users.noreply.github.com>
2025-03-20 20:02:41 +0530

    first commit

---
+++ app-op.js
@@ -0,0 +1,45 @@
+// let checklogin =()=>{
+//      let user = document.getElementById('uname').value;
+//      let pass = document.getElementById('pass').value;
+//      if((user == pass) && (user="myluu"))
+//      {
+//           alert("hii" + user+",login successfully..");
+//      }
+//  }
+
+// document.getElementById('btn').
+// addEventListener('click', checklogin);
+
+// let a=10;
+// ///a++;
+// console.log(a++);
+// console.log(a);
+// let b=15;
+// console.log(a+=b);
+// console.log(a*=b);
+
+// let age=20;
+// let result=(age>=18)?"Eligible to vote":"Not eligible to vote";
+// console.log(result);
+
+// let n=20;
+// for(let i=1;i<=n;i++)
+// {
+//     if(i%2!=0)
+//     {
+//     console.log(i);
+// }
+// }
+
+// let n=20;
+// for(let i=1;n>=i;n--)
+// {
+//     console.log(n);
+// }
+
+let n=10;
+let i=0;
+do{
+    i++;
+    console.log(i);
+}while(i<=n);
